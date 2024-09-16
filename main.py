74bf283019c199da2838c81fc38bba082eccf91b
jason routh <jasonrouth2005@gmail.com>
2024-09-15 20:34:08 -0400

    Transaction_result function updated and code formatted

--- main.py
+++ main.py
@@ -56,16 +56,23 @@ class SandwichMachine:
         """Returns the total calculated from coins inserted.
         Hint: include input() function here, e.g. input("how many quarters?: ")"""
         print("Please insert coins")
-        large_dollars = int(input("How many large dollars?: "))* 1.00
-        half_dollars = int(input("How many half dollars?: "))* 0.50
-        quarters = int(input("How many quarters?: "))* 0.25
-        nickels = int(input("How many nickels?: "))* 0.05
+        large_dollars = int(input("How many large dollars?: ")) * 1.00
+        half_dollars = int(input("How many half dollars?: ")) * 0.50
+        quarters = int(input("How many quarters?: ")) * 0.25
+        nickels = int(input("How many nickels?: ")) * 0.05
         total = large_dollars + half_dollars + quarters + nickels
         return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
         Hint: use the output of process_coins() function for cost input"""
+        if coins >= cost:
+            coins - cost
+            print(f"Here is ${change} in change.")
+            return True
+        else:
+            print("Sorry thats not enough money, Your money has been refunded")
+            return False
 
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
